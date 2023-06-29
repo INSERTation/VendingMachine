d79354b8781ac04203f3595a12de6973b921fc7a
INSERTation <62100292+INSERTation@users.noreply.github.com>
2023-06-29 23:49:36 +0800

    Add files via upload

--- VendingMachineFactory.java
+++ VendingMachineFactory.java
@@ -12,19 +12,13 @@ public class VendingMachineFactory {
             int choice = getUserChoice(1, 3);
 
             switch (choice) {
-                case 1:
-                    createVendingMachine();
-                    break;
-                case 2:
-                    testVendingMachine();
-                    break;
-                case 3:
+                case 1 -> createVendingMachine();
+                case 2 -> testVendingMachine();
+                case 3 -> {
                     exitProgram = true;
                     System.out.println("Exiting the program.");
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-                    break;
+                }
+                default -> System.out.println("Invalid choice. Please try again.");
             }
         }
 
@@ -47,60 +41,83 @@ public class VendingMachineFactory {
 
         if (choice == 1) {
             System.out.println("Enter the number of item slots for the Regular Vending Machine:");
-            int numSlots = getUserChoice(1, Integer.MAX_VALUE);
+            int numSlots = getUserChoice(1, Integer.MAX_VALUE); // make this 8 later
             currentVendingMachine = new RegularVendingMachine(numSlots);
             initializeRegularVendingMachine();
             System.out.println("Regular Vending Machine created.");
         } else if (choice == 2) {
             System.out.println("Enter the number of item slots for the Special Vending Machine:");
             int numSlots = getUserChoice(1, Integer.MAX_VALUE);
-            currentVendingMachine = new RegularVendingMachine(numSlots);
+            currentVendingMachine = new SpecialVendingMachine(numSlots);
             initializeSpecialVendingMachine();
             System.out.println("Special Vending Machine created.");
         }
     }
 
-    private static void initializeRegularVendingMachine() {
+        private static void initializeRegularVendingMachine() {
         for (int slotIndex = 0; slotIndex < currentVendingMachine.getNumSlots(); slotIndex++) {
-            System.out.println("Enter the name of the item for slot " + slotIndex + ":");
+            System.out.println("Enter the name of the item for slot " + (slotIndex + 1)+ ":");
             String itemName = scanner.nextLine();
-            System.out.println("Enter the quantity for slot " + slotIndex + ":");
+            System.out.println("Enter the quantity for slot " + (slotIndex + 1)+ ":");
             int quantity = getUserChoice(1, Integer.MAX_VALUE);
             currentVendingMachine.addItemSlot(slotIndex, new ItemSlot(itemName, quantity));
 
-            System.out.println("Enter the price for the item in slot " + slotIndex + ":");
+            System.out.println("Enter the price for the item in slot " +(slotIndex + 1)+ ":");
             double price = scanner.nextDouble();
             currentVendingMachine.setItemPrice(slotIndex, price);
 
-            System.out.println("Enter the calories for the item in slot " + slotIndex + ":");
+            System.out.println("Enter the calories for the item in slot " +(slotIndex + 1)+ ":");
             int calories = getUserChoice(0, Integer.MAX_VALUE);
             currentVendingMachine.setItemCalories(slotIndex, calories);
-            // Clear the newline character from input buffer
         }
     }
-    static class SpecialVendingMachine{
+    public static class SpecialVendingMachine extends RegularVendingMachine {
+        private String[][] recipes;
+        private String[][] preparationSteps;
 
         public SpecialVendingMachine(int numSlots) {
+            super(numSlots);
+            this.recipes = new String[numSlots][];
+            this.preparationSteps = new String[numSlots][];
+        }
 
+        public void addRecipe(int index, String[] recipe, String[] steps) {
+            recipes[index] = recipe;
+            preparationSteps[index] = steps;
+        }
+
+        public void prepareItem(int index) {
+            if (recipes[index] == null) {
+                System.out.println("Recipe not found. Dispensing as an individual item...");
+                // TODO: Add logic for dispensing individual items
+            } else {
+                String[] steps = preparationSteps[index];
+
+                for (String step : steps) {
+                    System.out.println(step + "...");
+                }
+
+                System.out.println("Item " + index + " Done!");
+            }
         }
     }
 
-        // Additional methods and properties specific to Special Vending Machine
+
 
 
     private static void initializeSpecialVendingMachine() {
         for (int slotIndex = 0; slotIndex < currentVendingMachine.getNumSlots(); slotIndex++) {
-            System.out.println("Enter the name of the item for slot " + slotIndex + ":");
+            System.out.println("Enter the name of the item for slot " + (slotIndex + 1)+ ":");
             String itemName = scanner.nextLine();
-            System.out.println("Enter the quantity for slot " + slotIndex + ":");
+            System.out.println("Enter the quantity for slot " + (slotIndex + 1)+ ":");
             int quantity = getUserChoice(1, Integer.MAX_VALUE);
             currentVendingMachine.addItemSlot(slotIndex, new ItemSlot(itemName, quantity));
 
-            System.out.println("Enter the price for the item in slot " + slotIndex + ":");
+            System.out.println("Enter the price for the item in slot " + (slotIndex + 1)+ ":");
             double price = scanner.nextDouble();
             currentVendingMachine.setItemPrice(slotIndex, price);
 
-            System.out.println("Enter the calories for the item in slot " + slotIndex + ":");
+            System.out.println("Enter the calories for the item in slot " + (slotIndex + 1)+ ":");
             int calories = getUserChoice(0, Integer.MAX_VALUE);
             currentVendingMachine.setItemCalories(slotIndex, calories);
 
@@ -120,19 +137,13 @@ public class VendingMachineFactory {
             int choice = getUserChoice(1, 3);
 
             switch (choice) {
-                case 1:
-                    testVendingFeatures();
-                    break;
-                case 2:
-                    testMaintenanceFeatures();
-                    break;
-                case 3:
+                case 1 -> testVendingFeatures();
+                case 2 -> testMaintenanceFeatures();
+                case 3 -> {
                     exitTest = true;
                     System.out.println("Returning to Test a Vending Machine menu.");
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-                    break;
+                }
+                default -> System.out.println("Invalid choice. Please try again.");
             }
         }
     }
@@ -154,16 +165,61 @@ public class VendingMachineFactory {
         System.out.println("Vending Features Test:");
         currentVendingMachine.displayInventory();
 
-        // Prompt user for choice and perform the necessary actions
+        // Let's add a test for inserting money
+        currentVendingMachine.insertMoney(5.0);
+
+        // Let's add a test for checking out an item
+        System.out.println("Choose a slot to checkout:");
+        int slotChoice = getUserChoice(0, currentVendingMachine.getNumSlots() - 1);
+        currentVendingMachine.checkout(slotChoice);
+
+        // Let's add a test for returning change
+        currentVendingMachine.returnChange();
 
         System.out.println("Vending Features Test completed.");
     }
 
     private static void testMaintenanceFeatures() {
-        System.out.println("Maintenance Features Test:");
-        // Implement maintenance features like restocking, setting price, collecting money, etc.
+        boolean running = true;
+
+        while (running) {
+            currentVendingMachine.displayInventory();
+
+            System.out.println("Maintenance Features Test:");
+            System.out.println("Choose an option:\n1. Restock\n2. Set price\n3. Collect money\n4. End maintenance");
+            int choice = getUserChoice(1, 4);
+
+            switch (choice) {
+                case 1 -> { // Restock
+                    System.out.println("Choose a slot to restock:");
+                    int slotChoice = getUserChoice(0, currentVendingMachine.getNumSlots() - 1);
+                    System.out.println("Enter the quantity to restock:");
+                    int quantity = getUserChoice(1, Integer.MAX_VALUE);
+                    currentVendingMachine.restock(slotChoice, quantity);
+                }
+                case 2 -> { // Set price
+                    System.out.println("Choose a slot to set price:");
+                    int slotChoice = getUserChoice(0, currentVendingMachine.getNumSlots() - 1);
+                    System.out.println("Enter the new price:");
+                    double price = scanner.nextDouble();
+                    currentVendingMachine.setPrice(slotChoice, price);
+                }
+                case 3 -> { // Collect money
+                    double collectedMoney = currentVendingMachine.collectMoney();
+                    System.out.println("Collected money: $" + collectedMoney);
+                }
+                case 4 -> { // End maintenance
+                    running = false;
+                }
+                default -> System.out.println("Invalid choice");
+            }
+        }
+
         System.out.println("Maintenance Features Test completed.");
     }
+
+
+
 
     private static int getUserChoice(int min, int max) {
         int choice;
@@ -197,6 +253,7 @@ class RegularVendingMachine {
     private int[] startingInventory;
     private int[] endingInventory;
     private int numSlots;
+    private double machineBalance;
 
     public RegularVendingMachine(int numSlots) {
         this.numSlots = numSlots;
@@ -205,6 +262,38 @@ class RegularVendingMachine {
         itemCalories = new int[numSlots];
         startingInventory = new int[numSlots];
         endingInventory = new int[numSlots];
+        this.machineBalance = 0.0;
+    }
+
+    public void insertMoney(double amount) {
+        this.machineBalance += amount;
+        System.out.println("Inserted $" + amount + ". Current balance: $" + this.machineBalance);
+    }
+
+    public void checkout(int slotIndex) {
+        if (isValidSlotIndex(slotIndex)) {
+            ItemSlot itemSlot = itemSlots[slotIndex];
+            double price = itemPrices[slotIndex];
+
+            if (itemSlot != null && itemSlot.getQuantity() > 0) {
+                if (this.machineBalance >= price) {
+                    this.machineBalance -= price;
+                    itemSlot.setQuantity(itemSlot.getQuantity() - 1);
+                    System.out.println("Dispensed " + itemSlot.getItemName() + ". Remaining balance: $" + this.machineBalance);
+                } else {
+                    System.out.println("Insufficient balance. Please insert more money.");
+                }
+            } else {
+                System.out.println("Item slot is empty. Please select a different slot.");
+            }
+        } else {
+            System.out.println("Invalid slot index.");
+        }
+    }
+
+    public void returnChange() {
+        System.out.println("Returning change: $" + this.machineBalance);
+        this.machineBalance = 0.0;
     }
 
     public void addItemSlot(int slotIndex, ItemSlot itemSlot) {
@@ -258,6 +347,36 @@ class RegularVendingMachine {
     public int[] getStartingInventory() {
         return startingInventory;
     }
+
+    public void restock(int slotChoice, int quantity) {
+        if (isValidSlotIndex(slotChoice)) {
+            ItemSlot itemSlot = itemSlots[slotChoice];
+            if (itemSlot != null) {
+                itemSlot.setQuantity(itemSlot.getQuantity() + quantity);
+                System.out.println("Restocked " + quantity + " items to slot " + slotChoice);
+            } else {
+                System.out.println("Item slot not initialized. Please initialize it first.");
+            }
+        } else {
+            System.out.println("Invalid slot index.");
+        }
+
+    }
+
+    public void setPrice(int slotChoice, double price) {
+        if (isValidSlotIndex(slotChoice)) {
+            itemPrices[slotChoice] = price;
+            System.out.println("Price set to $" + price + " for slot " + slotChoice);
+        } else {
+            System.out.println("Invalid slot index.");
+        }
+    }
+
+    public double collectMoney() {
+        double collectedMoney = this.machineBalance;
+        this.machineBalance = 0.0;
+        return collectedMoney;
+    }
 }
 
 class ItemSlot {
@@ -266,6 +385,9 @@ class ItemSlot {
 
     public ItemSlot(String itemName, int quantity) {
         this.itemName = itemName;
+        this.quantity = quantity;
+    }
+    public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
@@ -277,5 +399,6 @@ class ItemSlot {
         return quantity;
     }
 }
+
 
 
